bf837242981282c75363d4a7e922da3170c5cf60
Juliano Alves <von.juliano@gmail.com>
2020-06-28 16:19:18 +0100

    Missed the envs

--- src/main/scala/streaming/environment/Environments.scala
+++ src/main/scala/streaming/environment/Environments.scala
@@ -1,5 +1,17 @@
 package streaming.environment
 
+import streaming.environment.config.Configuration
+import streaming.environment.repository._
+import zio.ULayer
+import zio.clock.Clock
+
 object Environments {
 
+  type HttpServerEnvironment = Clock with Configuration
+  type AppEnvironment = HttpServerEnvironment with CitiesRepository
+
+  val httpServerEnvironment = Clock.live ++ Configuration.live
+  val dbTransactor: ULayer[DbTransactor] = Configuration.live >>> DbTransactor.h2
+  val citiesRepository: ULayer[CitiesRepository] = dbTransactor >>> CitiesRepository.live
+  val appEnvironment: ULayer[AppEnvironment] = httpServerEnvironment ++ citiesRepository
 }
